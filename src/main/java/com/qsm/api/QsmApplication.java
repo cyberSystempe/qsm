18bc8cb6b5595dd4569668adddcfd921c0b5b1cf
Bruno Freire da Silva <brunofreire@MacBook-Pro-de-Tiago.local>
2019-09-15 14:57:23 -0300

    Criando o Projeto

---
+++ src/main/java/com/qsm/api/QsmApplication.java
@@ -0,0 +1,13 @@
+package com.qsm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QsmApplication.class, args);
+	}
+
+}
